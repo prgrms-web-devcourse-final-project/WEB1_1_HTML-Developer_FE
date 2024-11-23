115f3486a2c63c8192e2bd2bc2b4af0c3fe5eeab
Kihoon Roh <started677@gmail.com>
2024-11-24 00:33:04 +0900

    chore: configure eslint and prettier settings

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const prettierConfig = {
+  printWidth: 100,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'auto',
+};
+
+export default prettierConfig;
