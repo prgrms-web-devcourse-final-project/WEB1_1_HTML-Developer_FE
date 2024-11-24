94a93aae05e63e9fcb942b347f50817643530b66
zelkovaria <hyein396@gmail.com>
2024-11-25 00:06:36 +0900

    feat: Set up the web app layout and styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
+// import './index.css';
 import App from './App';
 
+import GlobalStyle from 'styles/globalStyle';
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <>
+    <GlobalStyle />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </>
 );
