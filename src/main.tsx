7682a8bf798713547435bc6ea33cb471a50d20a7
nxnaxx <nxnaxx125@gmail.com>
2024-11-26 15:25:33 +0900

    feat: add initial storybook setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,14 @@ import { ThemeProvider } from '@emotion/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import './index.css';
 import App from './App';
 
-import GlobalStyle from 'styles/GlobalStyle';
 import theme from 'styles/theme';
 
 createRoot(document.getElementById('root')!).render(
   <>
     <StrictMode>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
         <App />
       </ThemeProvider>
     </StrictMode>
