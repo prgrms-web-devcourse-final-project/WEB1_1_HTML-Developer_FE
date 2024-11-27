6fdd2cad2b666b4301ca0cfa0af7f2962d183a02
zelkovaria <hyein396@gmail.com>
2024-11-27 23:17:13 +0900

    feat: add Select component and Storybook

---
+++ src/components/select/Select.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import Select from './Select';
+
+const meta = {
+  title: 'Select',
+  component: Select,
+  parameters: {
+    layout: 'centered',
+  },
+
+  tags: ['autodocs'],
+
+  argTypes: {
+    onClick: fn(),
+    children: { control: 'text' },
+  },
+} satisfies Meta<typeof Select>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    value: '서울',
+  },
+};
