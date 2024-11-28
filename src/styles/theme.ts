27e104304b5cac45f9a343858a996d395313d28f
Kihoon Roh <started677@gmail.com>
2024-11-29 00:33:17 +0900

    style: convert global typography values to rem

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -28,57 +28,57 @@ const colors = {
 
 const typography = {
   display: {
-    size: '2rem',
+    size: '3.2rem',
     lineHeight: 1.4,
     weight: 700,
   },
   header: {
-    size: '1.5rem',
+    size: '2.4rem',
     lineHeight: 1.4,
     weight: 600,
   },
   title1: {
-    size: '1.25rem',
+    size: '2rem',
     lineHeight: 1.4,
     weight: 600,
   },
   title2: {
-    size: '1.125rem',
+    size: '1.8rem',
     lineHeight: 1.4,
     weight: 500,
   },
   bodyR: {
-    size: '1rem',
+    size: '1.6rem',
     lineHeight: 1.6,
     weight: 400,
   },
   bodyM: {
-    size: '1rem',
+    size: '1.6rem',
     lineHeight: 1.4,
     weight: 500,
   },
   mediumButton: {
-    size: '1rem',
+    size: '1.6rem',
     lineHeight: 1.4,
     weight: 600,
   },
   smallButton: {
-    size: '0.875rem',
+    size: '1.4rem',
     lineHeight: 1.4,
     weight: 600,
   },
   caption: {
-    size: '0.875rem',
+    size: '1.4rem',
     lineHeight: 1.6,
     weight: 400,
   },
   chip: {
-    size: '0.875rem',
+    size: '1.4rem',
     lineHeight: 1.4,
     weight: 500,
   },
   smallText: {
-    size: '0.75rem',
+    size: '1.2rem',
     lineHeight: 1.4,
     weight: 400,
   },
@@ -88,7 +88,7 @@ const backdrop = {
   default: 'rgba(27, 29, 31, 0.6)',
 };
 
-const maxWidth = '430px';
+const maxWidth = '43rem';
 
 export type ColorsTypes = typeof colors;
 export type TypographyTypes = typeof typography;
