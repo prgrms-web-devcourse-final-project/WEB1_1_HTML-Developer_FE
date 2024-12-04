51935b757f51cc3c71394ca94a97fced85b4e2be
Kihoon Roh <started677@gmail.com>
2024-12-04 17:33:44 +0900

    feat: implement MyPage layout

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -1,5 +1,21 @@
+import styled from '@emotion/styled';
+
+import AccountStatus from './components/AccountStatus';
+import ManagementSection from './components/ManagementSection';
+import UserProfile from './components/UserProfile';
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <MyPageContainer>
+      <UserProfile />
+      <AccountStatus />
+      <ManagementSection />
+    </MyPageContainer>
+  );
 };
+
+const MyPageContainer = styled.div`
+  padding: 2.4rem;
+`;
 
 export default MyPage;
