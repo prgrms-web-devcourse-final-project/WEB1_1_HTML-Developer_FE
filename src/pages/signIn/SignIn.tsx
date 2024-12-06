03b17b72e458f826fb3aeb1422bb064663ebdbc9
zelkovaria <hyein396@gmail.com>
2024-12-06 14:37:37 +0900

    style: update kakaoURL

--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -5,10 +5,7 @@ import mainLogoTitle from 'assets/mainLogoTitle.svg';
 import { MediumButtonText } from 'styles/Typography';
 
 const SignIn = () => {
-  const Rest_api_key = import.meta.env.VITE_REST_API_KEY;
-  const redirect_uri = import.meta.env.VITE_REDIRECT_URI;
-
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const kakaoURL = `${import.meta.env.VITE_API_URL}/oauth2/login/kakao`;
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
