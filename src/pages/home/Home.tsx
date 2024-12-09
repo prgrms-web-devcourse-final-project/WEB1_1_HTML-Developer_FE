7d0b9140745c30a5d36313c643fae7686962bd0d
zelkovaria <hyein396@gmail.com>
2024-12-09 16:10:58 +0900

    refactor: extract MainPoster component from Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,15 @@
 import styled from '@emotion/styled';
 import { useQuery } from '@tanstack/react-query';
-import { LuCalendar } from 'react-icons/lu';
 import { TbChevronRight } from 'react-icons/tb';
 import { useNavigate } from 'react-router-dom';
-import { Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
+import MainPoster from './components/MainPoster';
+
 import { endPoint } from 'constants/endPoint';
 import RentalPostItem from 'pages/busRental/components/RentalPostItem';
-import { BodyRegularText, ChipText, HeaderText, TitleText2 } from 'styles/Typography';
+import { ChipText, TitleText2 } from 'styles/Typography';
 import { publicAxios } from 'utils';
 
 interface RentalPostItem {
@@ -42,7 +41,7 @@ const Home = () => {
     return response.data.result;
   };
 
-  const { data } = useQuery<Concert[]>({
+  const { data: concerts } = useQuery<Concert[]>({
     queryKey: ['mainConcerts'],
     queryFn: () => getMainConcert(),
   });
@@ -52,29 +51,12 @@ const Home = () => {
     queryFn: () => getMainRental(),
   });
 
-  if (!data) return null;
+  if (!concerts) return null;
 
   return (
     <HomeContainer>
-      <Swiper className="mySwiper" modules={[Pagination]} pagination={{ clickable: true }}>
-        {data.map((concert) => (
-          <SwiperSlide key={concert.id}>
-            <Poster>
-              <img alt="posterImg" src={concert?.poster} />
-              <PosterWrapper>
-                <HeaderText>{concert?.title}</HeaderText>
-                <ConcertDate>
-                  <LuCalendar size={20} />
-                  <BodyRegularText>
-                    {concert?.stdate} - {concert?.eddate}
-                  </BodyRegularText>
-                </ConcertDate>
-              </PosterWrapper>
-            </Poster>
-          </SwiperSlide>
-        ))}
-      </Swiper>
       <BustRental>
+        <MainPoster concerts={concerts} />
         <RentalHead>
           <TitleText2>공연 차량 대절</TitleText2>
           <div onClick={() => navigate('/bus-rental')}>
@@ -97,45 +79,7 @@ const Home = () => {
   );
 };
 
-const HomeContainer = styled.div`
-  .swiper-pagination-bullet {
-    background-color: ${({ theme }) => theme.colors.dark[200]};
-  }
-
-  .swiper-pagination-bullet-active {
-    width: 1.4rem;
-    border-radius: 1.2rem;
-    background-color: ${({ theme }) => theme.colors.dark[100]};
-  }
-`;
-
-const Poster = styled.div`
-  position: relative;
-  width: 100%;
-
-  img {
-    width: 100%;
-  }
-`;
-
-const PosterWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  gap: 1.2rem;
-  position: absolute;
-  bottom: 0;
-  height: 28rem;
-  width: 100%;
-  background: linear-gradient(to top, #1b1d1f, rgba(27, 29, 31, 0));
-  padding: 0 2.4rem 3.2rem 2.4rem;
-`;
-
-const ConcertDate = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.8rem;
-`;
+const HomeContainer = styled.div``;
 
 const BustRental = styled.div`
   padding: 2.4rem 2.4rem;
