784b6f502230b0297b87007bb704f9e1a8337a55
zelkovaria <hyein396@gmail.com>
2024-12-18 17:11:25 +0900

    feat: implement API connection on callback page

--- src/pages/callback/Callback.tsx
+++ src/pages/callback/Callback.tsx
@@ -1,4 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+import { publicAxios, tokenAxios } from 'utils';
+
+interface CallbackResultResponse {
+  isUser: boolean;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  accessToken: null | string;
+  refreshToken: null | string;
+}
+
+interface CallbackRequest {
+  timeStamp: string;
+  code: string;
+  message: string;
+  result: CallbackResultResponse;
+}
 const Callback = () => {
+  const params = new URL(document.URL).searchParams;
+  const code = params.get('code');
+
+  const getUserKakaoInfo = async (): Promise<CallbackRequest> => {
+    const { data } = await tokenAxios.get<CallbackRequest>('/auth/token/kakao', {
+      params: {
+        code: code,
+      },
+    });
+    return data;
+  };
+
+  const { data } = useQuery({
+    queryKey: ['kakaoLogin', code],
+    queryFn: getUserKakaoInfo,
+  });
+
+  useEffect(() => {
+    if (data) {
+      console.log('응답 data', data);
+    }
+  }, [data]);
+
   return <div>잠시만 기다려주세요</div>;
 };
 
--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -5,7 +5,11 @@ import mainLogoTitle from 'assets/mainLogoTitle.svg';
 import { MediumButtonText } from 'styles/Typography';
 
 const SignIn = () => {
-  const kakaoURL = `${import.meta.env.VITE_API_URL}/oauth2/login/kakao`;
+  const Rest_api_key = import.meta.env.VITE_REST_API_KEY;
+  const redirect_uri = import.meta.env.VITE_REDIRECT_URI;
+
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
